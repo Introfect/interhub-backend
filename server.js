4706f3c478ad5a2b9ef59a0af80d408131bdca1d
Aayushman03 <aayushmanpratap333@gmail.com>
2022-12-22 11:55:02 +0530

    git push

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require("express")();
 const server = require("http").createServer(app);
-import cors from "cors";
+const cors = require("cors");
 
 const io = require("socket.io")(server, {
 	cors: {
